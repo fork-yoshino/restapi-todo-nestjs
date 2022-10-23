9a06c10f564b3b6d939da976126effe8fe6b30e3
Yuui Yoshino <yoshino@fork.co.jp>
2022-10-23 10:29:50 +0900

    Feat nickNameの作成機能を実装

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async updateUser(userId: number, dto: UpdateUserDto) {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+    delete user.hashedPassword;
+    return user;
+  }
+}
